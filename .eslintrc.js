d7a0d7cbe16af29d7791e86060cd897272849048
WiseBrightness <1255437975@qq.com>
2024-03-09 13:49:11 +0800

    项目初始化。

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
-  root: true,
-  extends: ['@icebreakers/eslint-config-ts'],
-  overrides: [
-    {
-      files: ['*.ts', '*.mts', '*.cts', '*.tsx'],
-      rules: {
-        'no-undef': 'off'
-      }
-    }
-  ]
+    root: true,
+    extends: ['@icebreakers/eslint-config-ts'],
+    overrides: [
+        {
+            files: ['*.ts', '*.mts', '*.cts', '*.tsx'],
+            rules: {
+                'no-undef': 'off',
+            },
+        },
+    ],
 }
